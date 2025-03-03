341ad5736489b33d39dbf4ff2fe22170cf48f472
puneeth-jadhav <puneethkumar.jadhav@cloudangles.com>
2025-03-03 10:37:27 +0530

    Pushed the code from frontend ui

---
+++ schema.sql
@@ -0,0 +1,60 @@
+DROP DATABASE IF EXISTS flight_booking;
+            CREATE DATABASE flight_booking;
+            USE flight_booking;
+
+-- Create Tables
+
+CREATE TABLE USERS (
+    id INT PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    status ENUM('active', 'inactive'),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE TRANSACTIONS (
+    id INT PRIMARY KEY,
+    post_date DATE NOT NULL,
+    transaction_date DATE NOT NULL,
+    reference_number VARCHAR(100) UNIQUE NOT NULL,
+    merchant_data VARCHAR(150) NOT NULL,
+    dollar_amount DECIMAL NOT NULL,
+    close_date VARCHAR(50) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    user_id INT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE REPORTS (
+    id INT PRIMARY KEY,
+    close_date VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    total_amount DECIMAL NOT NULL,
+    user_id INT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
+);
+
+-- Add Foreign Key Constraints
+
+ALTER TABLE TRANSACTIONS
+ADD CONSTRAINT FK_TRANSACTIONS_USER_ID
+FOREIGN KEY (user_id) REFERENCES USERS(id)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE REPORTS
+ADD CONSTRAINT FK_REPORTS_USER_ID
+FOREIGN KEY (user_id) REFERENCES USERS(id)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE REPORTS
+ADD CONSTRAINT FK_REPORTS_TRANSACTION_ID
+FOREIGN KEY (transaction_id) REFERENCES TRANSACTIONS(id)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
